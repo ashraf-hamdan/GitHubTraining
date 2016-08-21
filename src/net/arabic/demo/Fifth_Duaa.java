dcf09a5f51ea93a1657b0c53b90b1439155273af
DuaaFateh <du3a_1990@hotmail.com>
2016-08-22 00:14:49 +0300

    Duaa Fateh Add Her Work

--- src/net/arabic/demo/Fifth_Duaa.java
+++ src/net/arabic/demo/Fifth_Duaa.java
@@ -1,15 +1,37 @@
 package net.arabic.demo;
 
+import android.R;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Fifth_Duaa extends Activity {
 
-	@Override
+	EditText ed1;
+	Button btn1;
+	String st1;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fifth__duaa);
+		btn1=(Button)findViewById(R.id.btnduaa);
+		ed1=(EditText)findViewById(R.id.edduaa);
+		Intent getintent1=this.getIntent();
+		st1=getintent1.getStringExtra("value");
+		btn1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Intent putint1= new Intent(Fifth_Duaa.this,Sixth_Fayrooz.class);
+				putint1.putExtra("value",st1);
+				startActivity(putint1);
+				
+			}
+		});
 	}
 
 	@Override
