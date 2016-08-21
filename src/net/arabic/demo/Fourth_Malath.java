624d622ca0966478aa043dcaa3349d18c903988e
malathhussien <malathhussien94@gmail.com>
2016-08-21 21:58:55 +0300

    malath commit

--- src/net/arabic/demo/Fourth_Malath.java
+++ src/net/arabic/demo/Fourth_Malath.java
@@ -2,14 +2,19 @@ package net.arabic.demo;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class Fourth_Malath extends Activity {
-
+ String value;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fourth__malath);
+		Intent info = this.getIntent();
+		value = info.getExtras().getString("value");
+		
 	}
 
 	@Override
@@ -18,5 +23,9 @@ public class Fourth_Malath extends Activity {
 		getMenuInflater().inflate(R.menu.fourth__malath, menu);
 		return true;
 	}
-
+  public void next(View v){
+	  Intent intent=new Intent(this,Fifth_Duaa.class);
+	  intent.putExtra("value",value);
+	  startActivity(intent);
+  }
 }
