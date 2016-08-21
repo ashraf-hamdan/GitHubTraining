9bbf91f4681d1a022a7f378fa1cdb7aab8581689
hla <hlahusain@gmail.com>
2016-08-21 20:12:56 +0300

    Third_Hala v3

--- src/net/arabic/demo/Third_Hala.java
+++ src/net/arabic/demo/Third_Hala.java
@@ -6,20 +6,28 @@ import android.view.Menu;
 
 public class Third_Hala extends Activity {
 
+	String value;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_third__hala);
 
         Intent info = getIntent();
-        String value = info.getStringExtra("value");
+        value = info.getStringExtra("value");
 
-        Intent intent = new Intent(this, Fourth_Malath.class);
-        intent.putExtra("value", value);
-        startActivity(intent);
 
 	}
 
+
+
+
+	public void clickNext(View view) {
+		Intent intent = new Intent(this, Fourth_Malath.class);
+		intent.putExtra("value", value);
+		startActivity(intent);
+
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
