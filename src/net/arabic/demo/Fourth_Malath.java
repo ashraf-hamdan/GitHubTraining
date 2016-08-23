2337d0e2271265e0dfeba2c41d03b6edf73fb804
ashraf-hamdan <you@example.com>
2016-08-23 14:02:31 -0700

    fixed

--- src/net/arabic/demo/Fourth_Malath.java
+++ src/net/arabic/demo/Fourth_Malath.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package net.arabic.demo;
 
 import android.os.Bundle;
@@ -8,41 +7,15 @@ import android.view.Menu;
 import android.view.View;
 
 public class Fourth_Malath extends Activity {
- String value;
+	String value;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fourth__malath);
 		Intent info = this.getIntent();
 		value = info.getExtras().getString("value");
-		
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.fourth__malath, menu);
-		return true;
-	}
-  public void next(View v){
-	  Intent intent=new Intent(this,Fifth_Duaa.class);
-	  intent.putExtra("value",value);
-	  startActivity(intent);
-  }
-}
-=======
-package net.arabic.demo;
-
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-
-public class Fourth_Malath extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_fourth__malath);
 	}
 
 	@Override
@@ -52,5 +25,9 @@ public class Fourth_Malath extends Activity {
 		return true;
 	}
 
+	public void next(View v) {
+		Intent intent = new Intent(this, Fifth_Duaa.class);
+		intent.putExtra("value", value);
+		startActivity(intent);
+	}
 }
->>>>>>> 782f8809378415f521716547f678de30d774302c
