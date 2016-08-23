82294e14d4692cd4aff11822d88236b6c31c03a5
ashraf-hamdan <you@example.com>
2016-08-23 14:21:35 -0700

    fixed Third Class

--- src/net/arabic/demo/Third_Hala.java
+++ src/net/arabic/demo/Third_Hala.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class Third_Hala extends Activity {
 
